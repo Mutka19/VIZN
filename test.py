67422e20f0a044338c266eea18afaa89ffad360e
Daemon Mutka <daemon.mutka98@gmail.com>
2023-11-26 13:07:37 -0600

    Attempting to refactor code to use boosted predict to test images

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 import os
-import cv2
+import cv2 as cv
 import pickle
 from config import data_directory, training_directory
-from boosting import integral_image, eval_weak_classifier
+from boosting import boosted_predict
+from skin_detection import detect_skin
 import importlib.util
 
 # Importing face annotations dynamically from a given file path
@@ -43,32 +44,36 @@ def calculate_iou(boxA, boxB):
     return iou
 
 # Detect faces in an image using a trained AdaBoost model
-def detect_faces(image, classifiers, extracted_classifiers, scale_factor=1.25, step_size=5):
+def detect_faces(image, boosted_classifiers, extracted_classifiers, scale_factor=1.25, step_size=5):
+    # Use skin detection to eliminate potential noise
+    rgb_img = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+    skin_img = detect_skin(rgb_img)
+
+    #Convert skin image to grayscale for skin detection
+    gray_img = cv.cvtColor(skin_img, cv.COLOR_RGB2GRAY)
+
     detected_faces = []
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     
     # Initial window size should match the trained classifier input
-    window_size = (50, 50)
+    window_size = (31, 25)
+
+    rows, cols = gray_img.shape
+
     current_scale = 1.0
     
     # Sliding window approach with scaling
-    while window_size[0] * current_scale < gray_image.shape[1] and window_size[1] * current_scale < gray_image.shape[0]:
+    while window_size[0] * current_scale < cols and window_size[1] * current_scale < rows:
         scaled_window_size = (int(window_size[0] * current_scale), int(window_size[1] * current_scale))
         
-        for y in range(0, gray_image.shape[0] - scaled_window_size[1], step_size):
-            for x in range(0, gray_image.shape[1] - scaled_window_size[0], step_size):
-                window = gray_image[y:y + scaled_window_size[1], x:x + scaled_window_size[0]]
-                resized_window = cv2.resize(window, window_size)
-                integral_window = integral_image(resized_window)
-                
-                # Classifier evaluation
-                sum_alpha = 0
-                for classifier, alpha, threshold in classifiers:
-                    feature = eval_weak_classifier(extracted_classifiers[classifier], integral_window)
-                    sum_alpha += alpha * feature
-                
+        for y in range(0, rows - scaled_window_size[1], step_size):
+            for x in range(0, cols - scaled_window_size[0], step_size):
+                window = gray_img[y:y + scaled_window_size[1], x:x + scaled_window_size[0]]
+                resized_window = cv.resize(window, window_size)
+
+                score = boosted_predict(resized_window, boosted_classifiers, extracted_classifiers)
+
                 # Face detection based on threshold
-                if sum_alpha >= threshold:
+                if score > 0:
                     detected_faces.append((x, y, x + scaled_window_size[0], y + scaled_window_size[1]))
         
         current_scale *= scale_factor
@@ -92,7 +97,7 @@ def main():
         photo_file_name = annotation['photo_file_name']
         true_faces = annotation['faces']
         image_path = os.path.join(data_directory, 'test_face_photos', photo_file_name)
-        image = cv2.imread(image_path)
+        image = cv.imread(image_path)
 
         if image is not None:
             detected_faces = detect_faces(image, classifiers, extracted_classifiers)
