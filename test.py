100a5c9c23cb600de65e533b700cd477c32b1de9
Daemon Mutka <daemon.mutka98@gmail.com>
2023-12-03 00:42:48 -0600

    Updated import for correct src paths

--- test.py
+++ test.py
@@ -1,12 +1,10 @@
 import os
 import cv2 as cv
 from config import data_directory
-from model import load_model
-from testing import detect_faces_cascade, calculate_iou, calculate_precision_recall, test_cropped_faces, test_nonfaces
+from src.model import load_model
+from src.testing import detect_faces_cascade, calculate_iou, calculate_precision_recall, test_cropped_faces, test_nonfaces
 from src.processing import load_test_images
-import matplotlib.pyplot as plt
 import importlib.util
-import numpy as np
 
 # Importing face annotations dynamically from a given file path
 def import_annotations(annotations_path):
