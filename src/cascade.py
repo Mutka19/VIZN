6a830990b5ac1e80bf9567913e4aa110c5f18515
dmutka <daemon.mutka98@gmail.com>
2023-12-06 23:12:16 -0600

    Quick fix for cascade training

--- src/cascade.py
+++ src/cascade.py
@@ -14,10 +14,10 @@ def train_cascade(faces, nonfaces, max_weak_count=1000, stages=12, initial_class
 
     stage = 0
 
-    while stage < stages - 1:
+    while stage < stages:
         print(f"Training stage {stage + 1}/{stages}")
         num_classifiers = initial_classifiers + stage * classifier_increment  # Increase classifiers in later stages
-        weak_count = max_weak_count // stages
+        weak_count = max_weak_count
         weak_classifiers = [generate_classifier(faces.shape[1], faces.shape[0]) for _ in range(weak_count)]
 
         nonface_integrals = np.array([integral_image(nonface) for nonface in nonfaces])  # Compute for current nonfaces
@@ -41,17 +41,19 @@ def train_cascade(faces, nonfaces, max_weak_count=1000, stages=12, initial_class
         # Run AdaBoost for this stage
         boosted_classifier = adaboost(responses, labels, num_classifiers)
 
-        if stage < stages - 1:
+        if stage < stages:
             # Update nonfaces with false positives for the next stage
             false_positives = get_false_positives(nonfaces, boosted_classifier, weak_classifiers)
-            if len(false_positives) < len(nonfaces) / 2.5 or len(false_positives) > len(nonfaces) / 1.05:
-                print("Stage training failed to produce classifier! Retrying!")
-                continue
-            elif len(false_positives) < len(nonfaces) / 1.05 and (len(false_positives) > len(nonfaces) / 2 or len(nonfaces) < 300):
+            if len(false_positives) < len(nonfaces) / 1.05 and (len(false_positives) > len(nonfaces) / 2 or len(nonfaces) < 300):
                 # Extract classifiers for this stage
                 cascade.append((boosted_classifier, weak_classifiers))
                 nonfaces = false_positives
                 stage+=1
+            else:
+                print("Stage training failed to produce classifier! Retrying!")
+                continue
+
+                
 
     return cascade
 
